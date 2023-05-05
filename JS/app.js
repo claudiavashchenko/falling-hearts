70363da622cc9f4ecfd7f8207a302a737fe586ea
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-05-05 10:29:32 -0500

    Added heart using js class

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,20 @@
 const body = document.querySelector("body");
+
+class Heart {
+    constructor(color, position, speed) {
+        this.color = color
+        this.position = position
+        this.speed = speed
+    }
+}
+
+function addHeart() {
+    const randomColor = "#000000"
+    const randonLeft = 40
+    const randomSpeed = 400
+
+    const newHeart = new Heart(randomColor, randonLeft, randomSpeed)
+    console.log(newHeart)
+}
+
+addHeart()
